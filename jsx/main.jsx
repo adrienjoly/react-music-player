3f586ba67158031994d3fd54855d3349017f7db4
Adrien Joly <adrien@whyd.com>
2015-07-18 16:44:49 +0200

    turned playemjs into a react component

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -3,49 +3,48 @@ window.DEEZER_APP_ID = 123456789; // TODO
 window.DEEZER_CHANNEL_URL = "http://mysite.com/deezer-channel.html"; // TODO
 window.JAMENDO_CLIENT_ID = "f9ff9f0f"; // TODO
 
-(function(){
-  var playerParams = {
-    playerId: "genericplayer",
-    origin: window.location.host || window.location.hostname,
-    playerContainer: document.getElementById("container")
-  };
-
-  require('playemjs');
-
-  window.makePlayem(null, null, function(){
-    document.getElementById("container").innerHTML = 'playem is ready';
-  });
-
-})();
-
-class Hello extends React.Component {
+class Playem extends React.Component {
   constructor() {
+    require('playemjs');
     this.state = {
-      opacity: 1.0
+      secs: 0,
+      status: "loading"
     };
   }
   componentDidMount() {
-    this.timer = setInterval(() => {
-      var opacity = this.state.opacity;
-      opacity -= .05;
-      if (opacity < 0.1) {
-        opacity = 1.0;
-      }
+    setInterval(()=>{
       this.setState({
-        opacity: opacity
+        secs: this.state.secs + 1
       });
-    }, 100);
+    }, 1000)
+    var playerParams = {
+      playerId: "genericplayer",
+      origin: window.location.host || window.location.hostname,
+      playerContainer: document.getElementById("container")
+    };
+    window.SOUNDCLOUD_CLIENT_ID = this.props.sc_api_key || "11f9999111b5555c22227777c3333fed"; // TODO
+    window.DEEZER_APP_ID = 123456789; // TODO
+    window.DEEZER_CHANNEL_URL = "http://mysite.com/deezer-channel.html"; // TODO
+    window.JAMENDO_CLIENT_ID = "f9ff9f0f"; // TODO
+    window.makePlayem(null, playerParams, () => {
+      this.setState({status: "ready"});
+    });
   }
   render() {
     return (
-      <div style={{opacity: this.state.opacity}}>
-        Hello {this.props.name}
+      <div>
+        <p>Playem component "{this.props.name}" has been running for {this.state.secs} seconds.</p>
+        <p>Current status: {this.state.status}</p>
+        <div id="container"></div>
       </div>
     );
   }
 }
 
 React.render(
-  <Hello name="world"/>,
+  <Playem
+    name="myplayer"
+    sc_api_key="11f9999111b5555c22227777c3333fed" // TODO
+  />,
   document.getElementById('react-container')
 );
