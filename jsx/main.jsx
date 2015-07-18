2cd87a90d15cb31744fc2ffcb3f864d2060cc442
Adrien Joly <adrien@whyd.com>
2015-07-18 16:02:00 +0200

    playemjs loads using browserify, from jsx code file

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -1,3 +1,23 @@
+window.SOUNDCLOUD_CLIENT_ID = "11f9999111b5555c22227777c3333fed"; // TODO
+window.DEEZER_APP_ID = 123456789; // TODO
+window.DEEZER_CHANNEL_URL = "http://mysite.com/deezer-channel.html"; // TODO
+window.JAMENDO_CLIENT_ID = "f9ff9f0f"; // TODO
+
+(function(){
+  var playerParams = {
+    playerId: "genericplayer",
+    origin: window.location.host || window.location.hostname,
+    playerContainer: document.getElementById("container")
+  };
+
+  require('playemjs');
+
+  window.makePlayem(null, null, function(){
+    document.getElementById("container").innerHTML = 'playem is ready';
+  });
+
+})();
+
 class ExampleApplication extends React.Component {
   render() {
     var elapsed = Math.round(this.props.elapsed  / 100);
@@ -12,6 +32,6 @@ var start = new Date().getTime();
 setInterval(() => {
   React.render(
     <ExampleApplication elapsed={new Date().getTime() - start} />,
-    document.getElementById('container')
+    document.getElementById('react-problem-notice')
   );
 }, 50);
